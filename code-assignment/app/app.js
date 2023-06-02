d29a002cdf27d1cf5a939633599ea8fdf09de5cf
Mark D Christensen <mark@brooksnet.com>
2023-06-02 17:01:32 -0600

    JavaScript Code for Button Click

--- code-assignment/app/app.js
+++ code-assignment/app/app.js
@@ -1,5 +1,13 @@
 
-$('.add-book').on('submit', () => { 
-    let values = $(this).serialize();
-    console.log(values);
-})
+const button = document.querySelector('#addbook');
+const table = document.querySelector('#mustread');
+
+console.log(button);
+button.addEventListener("click", () => {
+    const parm = window.location.search.substring(1).split("&");
+
+    console.log(parm);
+    }, 
+    false
+);
+
