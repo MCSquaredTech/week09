640ad42fd3c68d0a576c4d89735cc2d6d2b2cd12
Mark D Christensen <mark@brooksnet.com>
2023-06-06 08:30:38 -0600

    JavaScript for the Form

--- code-assignment/app/app.js
+++ code-assignment/app/app.js
@@ -1,13 +1,37 @@
 
-const button = document.querySelector('#addbook');
-const table = document.querySelector('#mustread');
+let insert = false; 
+console.log(window.location.search);
+let addBooks = () => { 
+    location.replace("add-book.html"); 
+    insert = true;
+}
 
-console.log(button);
-button.addEventListener("click", () => {
-    const parm = window.location.search.substring(1).split("&");
+if (window.location.search !== null) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let isbn = urlParams.get('ISBN');
+    let pubDate = urlParams.get('PubDate');
+    let title = urlParams.get('Title');
+    let urlPath = urlParams.get('URL');
+    let author = urlParams.get('Author');
+    let price = urlParams.get('Price');
+    
+    const books = document.getElementById('mustread');
+    const newRow = document.createElement('tr'); 
+    newRow.innerHTML = (`
+            <td>${isbn}</td>
+            <td>${pubDate}</td>
+            <td><a href=${urlPath} target="_blank">${title}</a></td>
+            <td>${author}</td>
+            <td>${price}</td>
+    `);
+    books.appendChild(newRow);
+    
+}
+    
 
-    console.log(parm);
-    }, 
-    false
-);
+
+
+    
+
 
